a6291cf2c8a242307611bc96dec487dd673de96c
조범규 <ycg01082@naver.com>
2020-06-16 09:33:55 +0900

    프로필, 게시판 변수 수정

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        class Meta:
-            model = User
-            fields = ("username", "password1", "password2")
+        model = get_user_model()
